9687e30684e262fff0411734034b44da1541fdce
Iain White <IainWhite@users.noreply.github.com>
2020-07-27 13:23:14 +1000

    Update c.c

--- c.c
+++ c.c
@@ -1,4 +1,32 @@
- int i = 0 ;  char B[88] ;
-  while ( i++ < 100 )
-    !sprintf( B, "%s%s", i%3 ? "":"Fizz", i%5 ? "":"Buzz" )
-    ? sprintf( B, "%d", i ):0, printf( ", %s", B );
+#include <stdio.h>
+
+void fizzbuzz(int);
+
+int main(void)
+{
+    for (int i = 1; i <= 100; i++) {
+        fizzbuzz(i);
+    }
+
+	return 0;
+}
+
+void fizzbuzz(int num)
+{
+    int printed = 0;
+
+    if (num % 3 == 0) {
+	    printf("Fizz");
+	    printed = 1;
+    }
+    if (num % 5 == 0) {
+	    printf("Buzz");
+	    printed = 1;
+    }
+
+    if (printed) {
+	    printf("\n");
+    } else {
+	    printf("%d\n", num);
+    }
+}
